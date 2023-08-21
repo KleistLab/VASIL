ef75460eaa95f7392f9c75c7bfe79c8f89a60513
AlexiaNomena <missralexia1@gmail.com>
2023-08-21 15:59:33 +0200

    works_on_files

--- scripts/Cross_neutralization/Cross_Neutralization.py
+++ scripts/Cross_neutralization/Cross_Neutralization.py
@@ -6,9 +6,9 @@ import pandas as pd
 import pickle
 
 
-### Load SpikeGroups and Mutation profile 
 from .util import cross_reactivity
 
+### Load SpikeGroups and Mutation profile 
 file1 = open("Data/SpikeGroups.pck", "rb") 
 SpikeGroups_list = pickle.load(file1)["names"]
 file1.close()
@@ -34,18 +34,71 @@ mut_x_sites_dic["Wuhan-Hu-1"] = []
 Cross_react_dic = {}
 AB = ""
 for ab in Ab_classes:
-    func_type = "MEAN"
     Cross_react_dic_ab, Missed, Greater_one = cross_reactivity(variant_x_names_cross, Escape_Fraction, [ab], 
-                                                          mut_x_sites_dic, EF_func = func_type, quiet = False)
+                                                          mut_x_sites_dic)
     
     Cross_react_dic_ab["variant_list"] = variant_x_names_cross
-    Cross_react_dic_ab["func_type"]= func_type
     Cross_react_dic[ab] = Cross_react_dic_ab[ab]
 
+"""Add FR to NTD-targeting AB assuming a FR of 10 to each mutations sites included in NTD Antigenic supersite"""   
+n = len(variant_x_names_cross)
+FR_NTB = np.ones((n, n))
+for i in range(n):
+    var_1 = variant_x_names_cross[i]
+    for j in range(n):
+        if i > j:
+            var_2 = variant_x_names_cross[j]
+
+            sites_1 = set(np.array(mut_x_sites_dic[var_1]).astype(int))
+            sites_2 = set(np.array(mut_x_sites_dic[var_2]).astype(int))
+
+            sites = list(sites_1.symmetric_difference(sites_2))
+            FR_sites = 1
+            for s in sites:
+                s = int(s)
+                if ((14<=s)&(s<=20)) or ((140<=s)&(s<=158)) or ((245<=s)&(s<=264)):
+                    FR_sites *= 10
+            FR_NTB[i, j] = FR_sites
+            FR_NTB[j, i] = FR_sites
+        
+Cross_react_dic["NTD"] = FR_NTB
+
 Cross_react_dic["variant_list"] = variant_x_names_cross
-Cross_react_dic["func_type"]= func_type
-file0 = open("Data/Cross_react_dic_pseudogroups_%d_%d.pck"%(start, stop), "wb") 
+file0 = open("Data/Cross_react_dic_spikegroups_%d_%d.pck"%(start, stop), "wb") 
 pickle.dump(Cross_react_dic, file0)
 file0.close()
 
 
+### Cross_reactivity containing delta validation generated previously
+file1 = open("Cross_react_dic_show.pck", "rb") # premade simulations
+Cross_react_dic_show = pickle.load(file1)
+variant_x_names_show = Cross_react_dic_show["variant_list"]
+file1.close()
+
+"""Add FR to NTD-targeting AB assuming a FR of 10 to each mutations sites included in NTD Antigenic supersite"""   
+n = len(variant_x_names_show)
+FR_NTB = np.ones((n, n))
+for i in range(n):
+    var_1 = variant_x_names_show[i]
+    for j in range(n):
+        if i > j:
+            var_2 = variant_x_names_show[j]
+
+            sites_1 = set(np.array(mut_x_sites_dic[var_1]).astype(int))
+            sites_2 = set(np.array(mut_x_sites_dic[var_2]).astype(int))
+
+            sites = list(sites_1.symmetric_difference(sites_2))
+            FR_sites = 1
+            for s in sites:
+                s = int(s)
+                if ((14<=s)&(s<=20)) or ((140<=s)&(s<=158)) or ((245<=s)&(s<=264)):
+                    FR_sites *= 10
+            FR_NTB[i, j] = FR_sites
+            FR_NTB[j, i] = FR_sites
+
+Cross_react_dic_show["NTD"] = FR_NTB
+Cross_with_delta_validation = Cross_react_dic_show
+Cross_react_dic_show["variant_list"] = variant_x_names_show
+file0 = open("Data/Cross_with_delta_validation.pck", "wb") 
+pickle.dump(Cross_with_delta_validation, file0)
+file0.close()
--- scripts/Expected_immunized/Expected_Immunized.py
+++ scripts/Expected_immunized/Expected_Immunized.py
@@ -1,3 +1,77 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+import pickle
+import re
+import joblib as jb
 
+from .util import Immunity_dynamics_fftconvolve, Antibody_ranges, Find_IC50_ranges, spikegroups_proportion
+
+"""Load Infection Data"""
+Population_Data_v0 = pd.read_csv('Data/caseAscertainmentTable.csv')
+Population_Data = pd.read_csv("Data/caseAscertainmentTable_reportedCasesRatio.csv")
+
+"""Load population data starting from July 1st, 2021"""
+Population_Data = Population_Data.drop(index = Population_Data.index[:list(Population_Data['date']).index("2021-07-01")])
+
+t = np.arange(1, len(Population_Data['date'])+1, 1) # array of timepoints at which to compute the antibody concentration
+infection_data_corrected = Population_Data['minNTrue'].values
+t_dates = Population_Data['date'].values
+days_incidence = list(Population_Data['date']) 
+Population_Data["pop"] = (Population_Data_v0["pop"].values[0])*np.ones(len(days_incidence))
+
+file1 = open("Data/Cross_with_delta_validation.pck", "rb") # premade simulations
+Cross_with_delta_validation = pickle.load(file1)
+variant_x_names_show = Cross_with_delta_validation["variant_list"]
+Cross_with_delta_validation.pop("variant_list")
+file1.close()
+
+file1 = open("Data/Cross_react_dic_spikegroups.pck", "rb") # Check that this is the file you want to load
+Cross_react_dic = pickle.load(file1)
+variant_x_names_cross = Cross_react_dic["variant_list"]
+file1.close()
+
+Ab_classes = list(Cross_react_dic.keys())
+
+"""Compute Antibody concentration over time for a range of t_half and t_max"""
+thalf_vec = np.linspace(25, 69, 15) 
+tmax_vec = np.linspace(14, 28, 5)
+c_t_vec, c_dframe_dic, dataname = Antibody_ranges(thalf_vec, tmax_vec, t, Ab_classes)
+IC50xx_dic, mean_IC50xx_dic = Find_IC50_ranges(thalf_vec, tmax_vec, t, Ab_classes,  Cross_with_delta_validation)
+
+
+file1 = open("Data/SpikeGroups.pck", "rb") 
+SpikeGroups_list = pickle.load(file1)["names"]
+file1.close()
+
+def ei_util(i):
+    variant_to_sim = [SpikeGroups_list[i]]
+    EI = {}
+    EI["Days"] = days_incidence
+    
+    for key in c_dframe_dic.keys():
+        PK_dframe = c_dframe_dic[key]
+        key_num = np.array(re.findall(r"\d+", key)).astype(int)
+
+        Res_sub_0 = Immunity_dynamics_fftconvolve(t, PK_dframe, infection_data = infection_data_corrected, 
+                                                     present_variant_list = SpikeGroups_list, 
+                                                     tested_variant_list =  variant_to_sim, 
+                                                     variant_name = variant_x_names_cross, 
+                                                     variant_proportion =  spikegroups_proportion, 
+                                                     Ab_classes = Ab_classes, 
+                                                     IC50xx= mean_IC50xx_dic,
+                                                     Cross_react_dic = Cross_react_dic, 
+                                                     )
+        
+        EI["t_half = %.3f \nt_max = %.3f"%(thalf_vec[key_num[0]], tmax_vec[key_num[1]])] = Res_sub_0
+        
+    """ Save Dynamics Without Vaccination """
+    EI_df = pd.DataFrame(EI)
+    EI_df.to_csv("Data/Immunized_SpikeGroup_%s_all_PK.csv"%variant_to_sim[0])
+
+
+try:
+    jb.Parallel(n_jobs = -1)(jb.delayed(ei_util)(i) for i in range(len(SpikeGroups_list)))    
+except:
+    jb.Parallel(n_jobs = -1, prefer = "threads")(jb.delayed(ei_util)(i) for i in range(len(SpikeGroups_list)))    
