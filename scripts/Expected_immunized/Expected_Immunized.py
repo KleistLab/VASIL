bb61ae94d67552fc5cd4e4feb74b07bfa93da5d5
AlexiaNomena <missralexia1@gmail.com>
2023-08-27 13:15:38 +0200

    re_adjust_for_config

--- scripts/Expected_immunized/Expected_Immunized.py
+++ scripts/Expected_immunized/Expected_Immunized.py
@@ -5,12 +5,13 @@ import pandas as pd
 import pickle
 import re
 import joblib as jb
+import sys
 
 from .util import Immunity_dynamics_fftconvolve, Antibody_ranges, Find_IC50_ranges
 
 """Load Infection Data"""
-Population_Data_v0 = pd.read_csv('Data/population_data.csv')
-Population_Data = pd.read_csv("Data/caseAscertainmentTable_reportedCasesRatio.csv")
+total_pop = sys.argv[1]
+Population_Data = pd.read_csv(sys.argv[2])
 
 """Load population data starting from July 1st, 2021"""
 Population_Data = Population_Data.drop(index = Population_Data.index[:list(Population_Data['date']).index("2021-07-01")])
@@ -19,17 +20,17 @@ t = np.arange(1, len(Population_Data['date'])+1, 1) # array of timepoints at whi
 infection_data_corrected = Population_Data['minNTrue'].values
 t_dates = Population_Data['date'].values
 days_incidence = list(Population_Data['date']) 
-Population_Data["pop"] = (Population_Data_v0["pop"].values[0])*np.ones(len(days_incidence))
+Population_Data["pop"] = total_pop * np.ones(len(days_incidence))
 
 
 """Load relevant cross_neutralization files"""
-file1 = open("Data/Cross_with_delta_validation.pck", "rb") # premade simulations
+file1 = open(sys.argv[3], "rb") # premade simulations
 Cross_with_delta_validation = pickle.load(file1)
 variants_x_names_show = Cross_with_delta_validation["variant_list"]
 Cross_with_delta_validation.pop("variant_list")
 file1.close()
 
-file1 = open("Data/Cross_react_dic_spikegroups.pck", "rb") # Check that this is the file you want to load
+file1 = open(sys.argv[4], "rb") # Check that this is the file you want to load
 Cross_react_dic = pickle.load(file1)
 variants_in_cross = Cross_react_dic["variant_list"]
 file1.close()
@@ -43,16 +44,19 @@ c_t_vec, c_dframe_dic, dataname = Antibody_ranges(thalf_vec, tmax_vec, t, Ab_cla
 IC50xx_dic, mean_IC50xx_dic = Find_IC50_ranges(thalf_vec, tmax_vec, t, Ab_classes,  Cross_with_delta_validation)
 
 
-file1 = open("Data/SpikeGroups.pck", "rb") 
+file1 = open(sys.argv[5], "rb") 
 SpikeGroups_list = pickle.load(file1)["names"]
 file1.close()
 
 ### Load SpikeGroup frequency already starts at July 1st, 2021 #####
-frequency_spk_df = pd.read_csv('Data/Daily_SpikeGroups_Freq.csv')
-frequency_spk_df.drop(columns = "Unnamed: 0", inplace = True)
+frequency_spk_df = pd.read_csv(sys.argv[6])
+try:
+	frequency_spk_df.drop(columns = "Unnamed: 0", inplace = True)
+except:
+	pass
 
 ### Make sure proportions axis 0 is aligned with Spikegroup_list
-spikegroups_freq= np.zeros((len(SpikeGroups_list), len(t)))
+spikegroups_freq = np.zeros((len(SpikeGroups_list), len(t)))
 for i in range(len(SpikeGroups_list)):
     spikegroups_freq[i, :] = frequency_spk_df[SpikeGroups_list[i]][:len(t)]
 
@@ -83,7 +87,7 @@ def ei_util(i):
         
     """ Save Dynamics Without Vaccination """
     EI_df = pd.DataFrame(EI)
-    EI_df.to_csv("Data/Immunized_SpikeGroup_%s_all_PK.csv"%variant_to_sim[0])
+    EI_df.to_csv(sys.argv[7]+"Immunized_SpikeGroup_%s_all_PK.csv"%variant_to_sim[0])
 
 
 try:
