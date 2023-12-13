fix frequency calculation

scripts/SpikeGroups_frequencies/Lineages_freqs.py
@@ -11,9 +11,10 @@ Created on Mon Nov 27 12:29:17 2023
 
 import pandas as pd
 import numpy as np
-from datetime import datetime
+from datetime import datetime, timedelta
 import joblib as jb
 from functools import partial
+from scipy.interpolate import interp1d
 import sys
 import pdb
 import re
@@ -26,20 +27,27 @@ except:
 	covsonar_data = pd.read_csv(sys.argv[1])
 
 try:
-    days_prop = covsonar_data["date"].values.astype(str)
+    days_prop0 = covsonar_data["date"].values.astype(str)
 except:
     sys.exit("date column not found in covsonar data")
-    
-lineages_all = covsonar_data["lineage"].values.astype(str)
+
+lineages_all_0 = covsonar_data["lineage"].values.astype(str)
 """Simulation timeframe"""
 date_start = sys.argv[2]
+
+inds_keep = np.array(days_prop0)!="nan"
+days_prop = np.array(days_prop0)[inds_keep]
+lineages_all = np.array(lineages_all_0)[inds_keep]
+if date_start in days_prop:
+    where_day = list(days_prop).index(date_start)
+    days_prop = days_prop[where_day:]
+    lineages_all = lineages_all[where_day:]
 
 """Start computing Variant-proportions"""
 # iniializing variant proportion for all lineages
 unique_days_prop_all = list(np.unique(days_prop))
 ### make sure that dates are sorted and not nan
 
-unique_days_prop_all = [x for x in unique_days_prop_all if x!= "nan"]
 unique_days_prop_sub = []
 for i in range(len(unique_days_prop_all)):
     try:
@@ -49,19 +57,52 @@ for i in range(len(unique_days_prop_all)):
         pass
 
 unique_days_prop_sub.sort(key = lambda date: datetime.strptime(date, "%Y-%m-%d")) 
-unique_days_prop_all = unique_days_prop_sub
-
-if date_start in unique_days_prop_all:
-    unique_days_prop = np.array(unique_days_prop_all[unique_days_prop_all.index(date_start):])
-else:
-    unique_days_prop = np.array(unique_days_prop_all)
-        
+unique_days_prop = unique_days_prop_sub
+ 
 def sub_func(s, x, days_prop, unique_days_prop, lineages_all, unique_lineage):
     res = np.sum((days_prop == unique_days_prop[s]) & (lineages_all == unique_lineage[x]))
     return res
 
 print("Timeline of lineage proportions: %s -- %s"%(unique_days_prop[0], unique_days_prop[-1]))
 unique_lineage_timeframe = np.unique(lineages_all)
+
+try:
+    seq_thres = int(sys.argv[4])
+except:
+    seq_thres = None
+    
+if seq_thres is not None:
+
+    num_seq = []
+    for u in range(len(unique_days_prop)):
+        count_u = np.sum(days_prop == unique_days_prop[u])
+        num_seq.append(count_u)
+    
+    i = 0
+    n_i = 0
+    days_prop_new = []
+    while i<len(unique_days_prop):
+        num_goal = 0
+        j = i
+        while num_goal <= seq_thres and j<len(num_seq):
+            num_goal += num_seq[j]
+            j +=1
+        grouped = days_prop[n_i:n_i+num_goal]
+        useq = np.unique(grouped)
+        days_prop_new += [useq[len(useq)//2]]*len(grouped)
+        i = j
+        n_i +=len(grouped)
+    
+
+    unique_days_prop_new = list(np.unique(days_prop_new))
+    if unique_days_prop_new[0]!=unique_days_prop[0]:
+        unique_days_prop_new = [unique_days_prop[0]] + unique_days_prop_new
+    if unique_days_prop_new[len(unique_days_prop_new) - 1]!=unique_days_prop[-1]:
+        unique_days_prop_new = unique_days_prop_new + [unique_days_prop[-1]]
+        
+    days_prop = np.array(days_prop_new)
+    unique_days_prop = np.array(unique_days_prop_new)
+
 """ Remove NONE and UNASSIGNED """
 """
 if "NONE" in unique_lineage_timeframe:
@@ -87,6 +128,22 @@ for x in range(len(unique_lineage_timeframe)):
     for s in range(len(unique_days_prop)):
         frequency_lineage[x, s] = res[s]
 
+"""Interpolate in-between dates"""
+if seq_thres is not None:
+    sdate = datetime.strptime(unique_days_prop[0], "%Y-%m-%d")
+    edate = datetime.strptime(unique_days_prop[len(unique_days_prop) - 1], "%Y-%m-%d")
+    date_list = pd.date_range(sdate,edate-timedelta(days=1),freq='d').strftime('%Y-%m-%d').tolist()
+    if unique_days_prop[len(unique_days_prop) - 1] not in date_list:
+        date_list.append(unique_days_prop[len(unique_days_prop) - 1])
+
+    x = np.array([date_list.index(d) for d in unique_days_prop])
+    y = frequency_lineage
+    sub_x = np.arange(0, len(date_list))
+    f = interp1d(x, y, axis = -1)
+    props_fill = f(sub_x)
+    unique_days_prop = np.array(date_list)
+    frequency_lineage = props_fill
+        
 """ Re-Normalize variant proportion data to reflect daily proportion distribution """
 NormProp = np.sum(frequency_lineage, axis = 0)
 freq_rounded = np.round(frequency_lineage,decimals = 10)
@@ -102,20 +159,22 @@ try:
     KW_all = list(covsonar_data["week_num"].values.astype(str))
     KW_kept = []
     for i in range(len(freq_dic["date"])):
-        KW_kept.append(KW_all[list(days_prop).index(freq_dic["date"][i])])
-        freq_dic["week_num"] = KW_kept       
+        if freq_dic["date"][i] in days_prop0:
+            KW_kept.append(KW_all[list(days_prop0).index(freq_dic["date"][i])])
+        else:
+            KW_kept.append(np.nan)
+    freq_dic["week_num"] = KW_kept       
 except:
     print("Consonar data does not have the column week_num of the calendar week")
     
 for x in range(len(unique_lineage_timeframe)):
     freq_dic[unique_lineage_timeframe[x]] = frequency_lineage[x, :]
 
-
 freq_df = pd.DataFrame(freq_dic, index = np.arange(0, len(freq_dic["date"])))
 freq_df.to_csv(sys.argv[3]) ### output file
 
-if len(sys.argv) > 3:
-    variant_mut_data = pd.read_csv(sys.argv[4])
+if len(sys.argv) > 5 :
+    variant_mut_data = pd.read_csv(sys.argv[5])
     variant_x_name_orig = np.array(variant_mut_data["lineage"].values).astype(str)
     mut_x_sites_orig = np.array(variant_mut_data["RBD_NTD_mutations"].values).astype(str)
     mut_x_sites_dic = {}
@@ -123,7 +182,7 @@ if len(sys.argv) > 3:
         mut_i = re.findall(r'\d+', mut_x_sites_orig[i])
         mut_x_sites_dic[variant_x_name_orig[i]] = mut_i
     
-    mut_file = open(sys.argv[5], "wb")
+    mut_file = open(sys.argv[6], "wb")
     pickle.dump({"positions": mut_x_sites_dic, "Group":variant_x_name_orig}, mut_file)
     mut_file.close()
     